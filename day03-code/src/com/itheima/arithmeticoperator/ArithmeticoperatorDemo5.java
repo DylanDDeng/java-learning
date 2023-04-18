0c96ad712e3681e915df36bc6a049739001d7741
ChengshengDeng <chengshengdeng0308@163.com>
2023-04-18 10:35:48 +0800

    a++ a--

--- day03-code/src/com/itheima/arithmeticoperator/ArithmeticoperatorDemo5.java
+++ day03-code/src/com/itheima/arithmeticoperator/ArithmeticoperatorDemo5.java
@@ -3,11 +3,13 @@ package com.itheima.arithmeticoperator;
 public class ArithmeticoperatorDemo5 {
     public static void main(String[] args) {
         // ++ 和 --
+        // a ++ 和 ++a 不赋值的时候效果一致
         int a = 10;
         a++;
         System.out.println(a); //11
         ++a;
         System.out.println(a); //12
+        // a ++ 和 ++a 不赋值的时候效果一致
         a--;
         System.out.println(a); //11
         --a;
