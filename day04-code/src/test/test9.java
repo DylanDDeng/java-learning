e0e7e2c61a3924a21152a71d6d090cbc48cffa3e
ChengshengDeng <chengshengdeng0308@163.com>
2023-04-26 11:44:16 +0800

    switch

--- day04-code/src/test/test9.java
+++ day04-code/src/test/test9.java
@@ -21,31 +21,15 @@ public class test9 {
         int week = sc.nextInt();
 
         // 2.利用switch进行星期匹配
-        switch(week){
-            case 1:
-                System.out.println("跑步");
-                break;
-            case 2:
-                System.out.println("游泳");
-                break;
-            case 3:
-                System.out.println("慢走");
-                break;
-            case 4:
-                System.out.println("动感单车");
-                break;
-            case 5:
-                System.out.println("拳击");
-                break;
-            case 6:
-                System.out.println("爬山");
-                break;
-            case 7:
-                System.out.println("好好吃一顿");
-                break;
-            default:
-                System.out.println("没有这个星期");
-                break;
+        switch (week) {
+            case 1 -> System.out.println("跑步");
+            case 2 -> System.out.println("游泳");
+            case 3 -> System.out.println("慢走");
+            case 4 -> System.out.println("动感单车");
+            case 5 -> System.out.println("拳击");
+            case 6 -> System.out.println("爬山");
+            case 7 -> System.out.println("好好吃一顿");
+            default -> System.out.println("没有这个星期");
         }
 
 
