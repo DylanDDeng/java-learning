0c779e288fa3423b9e9808c6a246891dcb6e545b
ChengshengDeng <chengshengdeng0308@163.com>
2023-05-04 13:42:19 +0800

    loop test7

--- day05-code/src/com/itheima/test/LoopTest7.java
+++ day05-code/src/com/itheima/test/LoopTest7.java
@@ -6,7 +6,12 @@ import java.util.Scanner;
 public class LoopTest7 {
     public static void main(String[] args) {
         // 需求： 程序自动生成一个1-100之间的随机数字，使用程序实现才出这个数字是多少
-        // 注意事项： 生成随机数的代码不能写在循环里面
+        // 扩展小需求： 3次提示猜不中，直接提示猜中了
+
+        // 注意事项：
+        // 1. 生成随机数的代码不能写在循环里面
+        // 2. 抽奖机制
+        int count = 0;
         Random r = new Random();
         int number = r.nextInt(100)+1; // 1~100
         // 猜这个数字是多少
@@ -14,6 +19,14 @@ public class LoopTest7 {
         while(true){
             System.out.println("请输入你要猜的数字");
             int guessNumber = sc.nextInt();
+
+            // 保底机制
+            count ++;
+            if(count == 3){
+                System.out.println("猜中了");
+                break;
+            }
+
             // 判断
             if(guessNumber > number){
                 System.out.println("大了");
