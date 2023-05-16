b05b73d5e0e7860b99861ae3256c366bad0b6d6c
ChengshengDeng <chengshengdeng0308@163.com>
2023-05-16 14:24:56 +0800

    MethodDemo5 & Test3

---
+++ day06-code/src/com/itheima/methoddemo/MethodDemo5.java
@@ -0,0 +1,32 @@
+package com.itheima.methoddemo;
+
+public class MethodDemo5 {
+    public static void main(String[] args) {
+        // 掌握带返回值的方法的定义和调用
+
+//        // 直接调用
+//
+//        getSum(30, 20, 10);
+//
+//        // 赋值调用
+//        int sum = getSum(30, 20, 10);
+//        System.out.println(sum);
+//
+//        // 输出调用
+//        System.out.println(getSum(30, 20, 10));
+        // 计算第一个季度的营业额
+        int sum1 = getSum(30, 20, 10);
+        int sum2 = getSum(40, 50, 10);
+        int sum3 = getSum(50, 30, 10);
+        int sum4 = getSum(60, 10, 10);
+
+        int sum = sum1 + sum2 + sum3 + sum4;
+
+        System.out.println(sum);
+
+    }
+    public static int getSum(int num1, int num2, int num3){
+        int result = num1 + num2 + num3;
+        return result;
+    }
+}
---
+++ day06-code/src/com/itheima/test/Test3.java
@@ -0,0 +1,21 @@
+package com.itheima.test;
+
+public class Test3 {
+    public static void main(String[] args) {
+        // 比较两个长方形的面积
+        double area1 = getArea(5.3,1.7);
+        double area2 = getArea(2.4, 2.7);
+
+        if (area1 > area2){
+            System.out.println("第一个长方形大");
+        }else{
+            System.out.println("第二个长方形更大");
+        }
+
+
+    }
+    public static double getArea(double len, double width){
+        double area = len * width;
+        return area;
+    }
+}
