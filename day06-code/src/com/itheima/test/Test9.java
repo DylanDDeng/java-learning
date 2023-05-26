956a3777345219da33ff03ace4066c3c5ee5b9ad
ChengshengDeng <chengshengdeng0308@163.com>
2023-05-26 17:15:23 +0800

    Test9&10

---
+++ day06-code/src/com/itheima/test/Test9.java
@@ -0,0 +1,14 @@
+package com.itheima.test;
+
+public class Test9 {
+    public static void main(String[] args) {
+        int number = 100;
+        System.out.println("调用方法前："+ number); // 100
+        change(number);
+        System.out.println("调用方法后：" + number); // 100
+
+    }
+    public static void change(int number){
+        number = 200;
+    }
+}
