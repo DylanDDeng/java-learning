df287b569f2fc62365aee5f45ff87953d51d1844
ChengshengDeng <chengshengdeng0308@163.com>
2023-06-29 12:55:26 +0800

    apidemo

---
+++ mystring/src/com/itheima/apidemo/ScannerDemo1.java
@@ -0,0 +1,16 @@
+package com.itheima.apidemo;
+
+import java.util.Scanner;
+
+public class ScannerDemo1 {
+    public static void main(String[] args) {
+        // 1. 创建对象
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入一个小数");
+        // 2. 接收一个小数
+        double result = sc.nextDouble();
+        // 3. 输出一个小数
+        System.out.println(result);
+
+    }
+}
